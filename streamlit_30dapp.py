40ef6f4d8f55d33d0f6fd556019afe0039824991
Binay <binzidd@gmail.com>
2023-03-26 13:00:06 +1100

    Update streamlit_30dapp.py

--- streamlit_30dapp.py
+++ streamlit_30dapp.py
@@ -11,8 +11,29 @@ else:
 	st.write('Goodbye')
 
 ## add a sidebar 
+##sidebar object notation 
 
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
 )
+
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
